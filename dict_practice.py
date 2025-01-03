8910386bbea2f1bba2e95217130b4b5e7c7fcdf6
asad ansari <ansariboy385@gmail.com>
2025-01-03 19:13:03 +0000

    creat new file

--- dict_practice.py
+++ dict_practice.py
@@ -1,3 +1,5 @@
 record = {"name" : "Asad", "age" : 26, "city":"fsd"}
 
 print(record.get("name"),record.get("age"),record.get("city"))
+
+print("hello world")
--- list_practice.py
+++ list_practice.py
@@ -1,3 +1,5 @@
 name = [1,2,3,4.5]
 
 print(name[0:3])
+
+print("happy new year")
